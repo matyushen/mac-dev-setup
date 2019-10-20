3ce69154b3df6af66ed5a18111d33ddadf499cd1
Nikita Matyushenko <matyushen@users.noreply.github.com>
2019-10-20 13:07:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,50 @@
 #!/bin/bash
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+
+# Install cask
+brew tap caskroom/cask
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+# Add permissions to user (or group) under this folders and enclosed items
+sudo chown -R $(whoami) /usr/local/opt
+sudo chown -R $(whoami) /usr/local/share
+
+# WARNING: Add next permission just in case you machine is used by only one user.
+sudo chown -R $(whoami) /usr/local/lib
+
+# Install node through NVM
+brew install nvm
+mkdir ~/.nvm
+nvm install --lts
+nvm alias default stable
+nvm use default
+
+# Update NPM
+npm i -g npm
+
+# Other brew pakcages
+brew install yarn git zsh hub
+
+# git config --global core.editor 'code --wait'
+
+# Install Fira font (it has powerline support built in)
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+
+# Use the Homebrew version of ZSH
+chsh -s /usr/local/bin/zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install apps
+brew cask install docker iterm2 google-chrome visual-studio-code slack spotify ngrok spectacle 1password hyper graphql-playground webstorm github appcleaner figma firefox homebrew/cask-versions/firefox-developer-edition
+
+# Install setting sync extension for vs code
+code --install-extension shan.code-settings-sync
