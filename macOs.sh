f52f7b8af5eb7914e35411f81abd2917d56c145f
Nikita Matyushenko <matyushen@users.noreply.github.com>
2024-07-05 21:31:02 +0200

    Update macOs.sh

--- macOs.sh
+++ macOs.sh
@@ -1,61 +1,169 @@
 #!/bin/bash
 
-echo "Setting MacOS..."
+# Request administrator password upfront
+sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "Setting up your Mac..."
+
+##########################
 # FINDER
-# Do not show icons for hard drives, servers, and removable media on the desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+##########################
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show the ~/Library folder
+# chflags nohidden ~/Library
+
+# Show path bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show status bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
+
 # Use column view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string "Clmv"
-# TODO: Minimize icon intio application icon
-# TODO: Finder: When performing search, search current folder
+
+# Do not show icons for hard drives, servers, and removable media on the desktop
+# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+
+# Minimize icon intio application icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# When performing search, search current folder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 # TODO: New finder window show User folder
 
+##########################
 # DOCK
-#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+##########################
+
+# Set the icon size of Dock items to 36 pixels
 defaults write com.apple.dock tilesize -int 36
-#"Speeding up Mission Control animations and grouping windows by application"
+
+# Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Group windows by application
 defaults write com.apple.dock "expose-group-by-app" -bool true
-#"Setting Dock to auto-hide and removing the auto-hiding delay"
+
+# Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
+
 # Use the scale effect
 defaults write com.apple.dock mineffect -string scale
-# TODO: Do not show recent applications in Dock
 
+# Do not show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+##########################
 # TRACKPAD
-# Setting trackpad & mouse speed to a reasonable number
+##########################
+
+# Set fast trackpad and mouse tracking speed
 defaults write -g com.apple.trackpad.scaling 2
 defaults write -g com.apple.mouse.scaling 2.5
+
 # Disable 'natural' (Lion-style) scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
 # TODO: Tap to ckick
 
-# VAR
-# TODO: Set default code editor
-# TODO: Enable night shift
-# Expanding the save panel by default
+##########################
+# VARIOUS
+##########################
+
+# Disable boot sound
+sudo nvram SystemAudioVolume=" "
+
+# Show battery percentage in the menu bar
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Enable Night Shift from sunset to sunrise
+defaults write com.apple.CoreBrightness CBBlueReductionStatus -int 1
+
+# Expand the save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
 # Enabling subpixel font rendering on non-Apple LCDs
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
 # Enabling snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
 # Preventing Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-# Setting screenshots location to ~/Pictures/Screenshots
-# TODO: Create the folder first
-mkdir Pictures/Screenshots
-defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
-# Save screenshots in PNG format 
-defaults write com.apple.screencapture type -string "png"
+
+# Tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+##########################
+# ENABLE FILEVAULT DISK ENCRYPTION
+##########################
+
+sudo fdesetup enable
+
+##########################
+# ENABLE FIREWALL
+##########################
+
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+
+# Disable specific Spotlight categories
+SPOTLIGHT_CATEGORIES=(
+    "APPLICATIONS"
+    "SYSTEM_PREFS"
+    "DIRECTORIES"
+    "PDF"
+    "FONTS"
+    "DOCUMENTS"
+    "MESSAGES"
+    "CONTACT"
+    "EVENT_TODO"
+    "IMAGES"
+    "BOOKMARKS"
+    "MUSIC"
+    "MOVIES"
+    "PRESENTATIONS"
+    "SPREADSHEETS"
+    "SOURCE"
+)
+for CATEGORY in "${SPOTLIGHT_CATEGORIES[@]}"; do
+    defaults write com.apple.spotlight orderedItems -array-add "{enabled = 0; name = \"${CATEGORY}\";}"
+done
+
+# Restart Spotlight to apply changes
+killall mds
+sudo mdutil -i on /
+sudo mdutil -E /
 
 killall Finder
 killall Dock
